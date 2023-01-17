76b21aac70ed292144b5da0e72caa41c1d6f3e12
ihtshamali <ihtshamali18@gmail.com>
2023-01-17 13:23:44 +0500

    New Updates 17/1/23

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "faccb681aa7b7c0f"; }
+/******/ 		__webpack_require__.h = function() { return "5cee2ad7db1420b3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/ai.js
+++ pages/ai.js
@@ -3,12 +3,15 @@ import Head from "next/head"
 
 function Ai() {
   return (
+    <>
     <Head>
         <title>AI</title>
         <meta name="description" content="Artifical Intelligence" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon-mtsn.png" />
       </Head>
+      <h1>AI</h1>
+      </>
   )
 }
 
