b5618fcc2043ac993c5e303b02afabd5ac5e3227
Ihtsham Ali <106303163+ihtshamali@users.noreply.github.com>
2023-04-04 22:14:07 +0500

    Update memberships.js

--- pages/components/Home/membership/memberships.js
+++ pages/components/Home/membership/memberships.js
@@ -21,7 +21,7 @@ function Memberships() {
             <section style={{ backgroundColor: '#151720' }} className='py-5 py-mobile-0'>
                 <Container>
                     <h2 className='fs40 py-4 px-lg-0 mb-2 text-center text-white'>Cloud Membership</h2>
-                    <h3 style={{ color: 'rgba(156, 153, 153, 1)' }} className='fs28 mb-5 text-center'>Cut costs, not corners</h3>
+                    <h3 style={{ color: 'rgba(156, 153, 153, 1)' }} className='fs28 mb-4 text-center'>Cut costs, not corners</h3>
                     <p style={{ color: 'rgba(156, 153, 153, 1)' }} className='fs18 px-xxl-95 text-center mb-5 mb-lg-12'>
                         Utilizing cloud-based remote work allows businesses to access top-tier development talent
                         from around the world at a more affordable cost compared to building an in-house team.
@@ -31,8 +31,8 @@ function Memberships() {
                         <span></span>
                         <div className='px-12 pb-12 pb-md-0 col-md-6'>
                             <div className='text-center'>
-                                <h4 className="fs28 mb-5 mb-md-12 text-white">Development is challenging</h4>
-                                <ul style={{ color: 'rgba(156, 153, 153, 1)' }} className="fs18 list-unstyled mb-5">
+                                <h4 className="fs28 mb-4 mb-md-12 text-white">Development is challenging</h4>
+                                <ul style={{ color: 'rgba(156, 153, 153, 1)' }} className="fontsize18 list-unstyled mb-5">
                                     <li>Hiring a local development team can be costly, while outsourced</li>
                                     <li>talent may not always meet expectations. This can leave </li>
                                     <li>businesses feeling stuck and overwhelmed, rather than </li>
@@ -43,8 +43,8 @@ function Memberships() {
                         </div>
                         <div className='px-12 col-md-6'>
                             <div className='text-center'>
-                                <h4 className="fs28 mb-5 mb-md-12 text-white">MTSN makes things easier</h4>
-                                <ul style={{ color: 'rgba(156, 153, 153, 1)' }} className="fs18 list-unstyled mb-0">
+                                <h4 className="fs28 mb-4 mb-md-12 text-white">MTSN makes things easier</h4>
+                                <ul style={{ color: 'rgba(156, 153, 153, 1)' }} className="fontsize18 list-unstyled mb-0">
                                     <li>MTSN teams work 100% remote to cut costs for our clients</li>
                                     <li>All MTSN developers and designers have a track record of success </li>
                                     <li>We provide the ultimate blend of qualified tech talent and affordability</li>
